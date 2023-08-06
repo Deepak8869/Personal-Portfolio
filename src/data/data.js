4665a1d3ad6dc8ce9f05d6032bac2bd9da8922e4
Deepak8869 <deepak8869880049@gmail.com>
2023-08-06 10:46:56 +0530

    portfolio

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import Project1 from '../assets/projects/Project1.png'
+import Project3 from "../assets/projects/project3.png"
+import Project4 from "../assets/projects/project4.png";
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Project1,
+        github:"https://github.com/Deepak8869/FrontendReact-project",
+        live:"https://graduateburgerwala.netlify.app",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Project3,
+        github:"https://github.com/Deepak8869/Tictactoe-project-",
+        live:"https://deepak8869.github.io/Tictactoe-project-/",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Project4,
+        github:"https://github.com/Deepak8869/Detective-project",
+        live:"https://deepak8869.github.io/Detective-project/",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
